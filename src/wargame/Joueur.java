93adc5901115d2792386823ea431080a44ee64c0
Hanybay <hanybayazid11@gmail.com>
2022-11-24 18:57:11 +0100

    ajout des getters + setters dans Joueur

--- src/wargame/Joueur.java
+++ src/wargame/Joueur.java
@@ -3,6 +3,19 @@ package wargame;
 public class Joueur {
 	// Infos
 	private String pseudo;
-	private final char CAMP;
+	private final char CAMP = 'M'; // méchant pour l'instant pour pas avoir d'erreur
 	private Armee arm;
+	
+	// Getters
+	public String getPseudo() {return pseudo;}
+	public Armee getArm() {return arm;}
+	public char getCAMP() {return CAMP;}
+	
+	// Setters
+	public void setPseudo(String pseudo) {this.pseudo = pseudo;}
+	public void setArm(Armee arm) {this.arm = arm;}
+	
+	
+	
+	
 }
