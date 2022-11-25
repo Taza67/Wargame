4211973de50db7f9944897a1cc44a49644863976
Taza67 <tazaakil67@gmail.com>
2022-11-25 11:30:46 +0100

    Suppression des anciens fichiers

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,24 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
 import wargame.*;
 
 public class Test {
-	Carte c = new Carte();
-	
 	public static void main(String[] args) {
+		JFrame frame = new JFrame("Test du jeu");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Panneau panel = new Panneau();
+		panel.setOpaque(true);
+		panel.setBackground(IConfig.COULEUR_VIDE);
+		panel.setPreferredSize(new Dimension(IConfig.LARGEUR_CARTE * IConfig.NB_PIX_CASE + 5, IConfig.HAUTEUR_CARTE * IConfig.NB_PIX_CASE + 5));
+		
+		frame.setContentPane(panel);
+		frame.setLocation(IConfig.POSITION_X, IConfig.POSITION_Y);
+		
+		frame.pack();
+		frame.setVisible(true);
 	}
+
 }
