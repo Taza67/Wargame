6a94f33c3bb9dff5d3fe0a5c576f9e487ed90f8a
Ryuma69 <azedine.amirouche@gmail.com>
2023-01-10 16:19:06 +0100

    javadoc APatient

--- src/wargame/APatient.java
+++ src/wargame/APatient.java
@@ -2,12 +2,34 @@ package wargame;
 
 import java.util.List;
 
+import wargameInterface.Fenetre;
+
+
+/**<b>Apatient est l'interface qui gère les processus .</b>
+* <p>
+* Elle est caractérisée par :
+* <ul>
+* <li>Une liste de processus.</li>
+* </ul>
+* </p>
+* @see AttaqueSoldatCorps
+* @author AKIL M., BAYAZID H., AMIROUCHE A.
+*/
+
+
 public abstract class APatient extends Thread {
-	// Infos
+	/**
+	 * La liste des processus
+	 * @see DeplacementSoldat
+	 */
 	protected List<Thread> processusAttendre;
 	
-	// Méthodes
-	// Attend la fin des threads à attendre
+	
+	/**
+	 * Attend la fin des threads
+	 * 
+	 * @see TourOrdi
+	 */
 	public void attendre() {
 		while(MethodesAuxiliaires.threadVivant(processusAttendre));
 	}
