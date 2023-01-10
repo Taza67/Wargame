5f013b50f5c79728623dd37ce5280e8f16d320b3
Ryuma69 <azedine.amirouche@gmail.com>
2023-01-10 17:19:28 +0100

    javadoc Héros.java

--- src/wargame/Heros.java
+++ src/wargame/Heros.java
@@ -3,13 +3,48 @@ package wargame;
 import java.awt.Font;
 import java.awt.Graphics2D;
 
+
+
+
+
+/**
+ * <b> Heros est la classe qui gère les personnages joueur, les positionne sur la map, les dessine, leur permet de se déplacer.</b>
+ * <p>
+ * Elle est caractérisée par :
+ * <ul>
+ * <li>Un nom</li>
+ * <li>Un type</li>
+ * <li>Une texture</li>
+ * </ul>
+ * </p>
+ * @author AKIL M., BAYAZID H., AMIROUCHE A.
+ *
+ */
+
+
+
 public class Heros extends Soldat {
 	private static final long serialVersionUID = -3558011063868665290L;
-	// Infos
+	
+	
+	/**
+	 * Nom du héros
+	 **/	
+	
 	private final String NOM;
+	 /**
+	  * Type du Héros, elfe, nain, humain, hobbit
+	  * 
+	  */
 	private final TypesH TYPE;
 	
-	// Constructeur
+	/**
+	 * Constructeur d'un héros
+	 * @param carte
+	 * @param type
+	 * @param nom
+	 * @param pos
+	 */
 	public Heros(Carte carte, TypesH type, String nom, Position pos) {
 		super(carte, pos, type.getPoints(), type.getPorteeVisuelle(), type.getPorteeDeplacement(), type.getPuissance(), type.getTir());
 		NOM = nom; TYPE = type;
@@ -18,12 +53,21 @@ public class Heros extends Soldat {
 		this.getZoneVisuelle().rendreVisible();
 	}
 	
-	// Accesseurs
+	/**
+	 * Accesseur du nom 
+	 * @return String
+	 */
 	public String getNOM() { return NOM; }
+	/**
+	 * Accesseur du type du héro
+	 * @return TypesH
+	 */
 	public TypesH getTYPE() { return TYPE; }
 	
-	// Méthodes
-	// Déplace le soldat à la position pos
+	
+	/**
+	 * Renvoie vrai si le déplacement est possible 
+	 */
 	public boolean seDeplace(Position cible) {
 		boolean possible = super.seDeplace(cible);
 		if (possible) {
@@ -32,13 +76,22 @@ public class Heros extends Soldat {
 		}
 		return possible;
 	}
-	// Renvoie le type de l'élément sous forme de chaine de caractère
+	
+	
+	/**
+	 * Retourne le type du héros sous forme de chaine
+	 * @return String
+	 */
 	public String getStringType() {
 		return ("" + TYPE).toLowerCase();
 	}
 	
 	// Méthodes graphiques
 	// Dessine le héros avec son nom
+	
+	/**
+	 * Dessine le héros
+	 */
 	public void seDessiner(Graphics2D g) {
 		super.seDessiner(g);
 		drawCenteredString(g, NOM, new Font("Courier", Font.BOLD, carte.getRayonHex()));
