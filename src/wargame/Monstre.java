4263d90a81b4d8c1fd0a1e31c729d73eb7a7bac7
Ryuma69 <azedine.amirouche@gmail.com>
2023-01-10 19:25:59 +0100

    javadoc Monstre

--- src/wargame/Monstre.java
+++ src/wargame/Monstre.java
@@ -1,21 +1,44 @@
 package wargame;
 
+/**
+ * Classe monstre extension de la classe Soldat
+ * <p>
+ * Est caractérisé par :
+ * <ul>
+ * <li>Un type de monstre </li>
+ * </ul>
+ * </p>
+ *
+ */
+
 public class Monstre extends Soldat {
 	private static final long serialVersionUID = -4592881709041950606L;
-	// Infos
+	/**
+	 * Type du Monstre
+	 */
 	private final TypesM TYPE;
 	
-	// Constructeur
+	/**
+	 * Constructeur de Monstre
+	 * @param carte
+	 * @param type
+	 * @param pos
+	 */
 	public Monstre(Carte carte, TypesM type, Position pos) {
 		super(carte, pos, type.getPoints(), type.getPorteeVisuelle(), type.getPorteeDeplacement(), type.getPuissance(), type.getTir());
 		TYPE = type;
 		numTexture = TEX_MONSTRE;
 	}
 	
-	// Accesseurs
+	/**
+	 * Accesseur du type
+	 * @return TypesM
+	 */
 	public TypesM getTYPE() { return TYPE; }
 	
-	// Méthodes
+	/**
+	 * Retourne le type du personnage sous forme de chaine de caractère
+	 */
 	public String getStringType() {
 		return ("" + TYPE).toLowerCase();
 	}
