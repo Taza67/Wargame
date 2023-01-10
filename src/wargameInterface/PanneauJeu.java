8c3fac31207092734c3c9d2ac5112ddd5364051b
Ryuma69 <azedine.amirouche@gmail.com>
2023-01-10 22:45:34 +0100

    javadoc PanneauJeu

--- src/wargameInterface/PanneauJeu.java
+++ src/wargameInterface/PanneauJeu.java
@@ -2,22 +2,42 @@ package wargameInterface;
 
 import java.awt.Dimension;
 
+
+
 import javax.swing.JPanel;
 
 import wargame.Carte;
 
+
+
+/**
+ * <b>Classe PanneauJeu extention de JPanel</b>
+ * <p>
+ * Elle est caractérisée par :
+ * <ul>
+ * <li>Un PanneauGrille grille</li>
+ * </ul>
+ * </p>
+ * 
+ * @author AKIL M., BAYAZID H., AMIROUCHE A.
+ *
+ */
+
 public class PanneauJeu extends JPanel {
 	private static final long serialVersionUID = 1L;
-	// Infos
+	/**
+	 * PanneauGrille grille
+	 */
 	protected PanneauGrille grille;
 	
+	/**
+	 * Constructeur PanneauJeu
+	 * @param carte
+	 */
 	public PanneauJeu(Carte carte) {
 		super();
 		this.grille = new PanneauGrille(carte);
 		this.add(grille);
-		// this.setLayout((LayoutManager)new BorderLayout());
-		// this.add(grille, BorderLayout.NORTH);
-		// this.add(infosCurseur, BorderLayout.SOUTH);
 		this.setPreferredSize(new Dimension(Carte.largeurMap, Carte.hauteurMap));
 	}
 }
