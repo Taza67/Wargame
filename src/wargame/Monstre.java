9984ec371b93a52a63ae3c5a8f2534a68bf1b17d
Hanybay <hanybayazid11@gmail.com>
2023-01-10 20:53:30 +0100

    Javadoc Obstacle

--- src/wargame/Monstre.java
+++ src/wargame/Monstre.java
@@ -1,22 +1,45 @@
 package wargame;
 
+/**
+ * Classe monstre extension de la classe Soldat
+ * <p>
+ * Est caractérisé par :
+ * <ul>
+ * <li>Un type de monstre </li>
+ * </ul>
+ * </p>
+ *
+ */
+
 public class Monstre extends Soldat {
-	private static final long serialVersionUID = -4592881709041950606L;
-	// Infos
-	private final TypesM TYPE;
-	
-	// Constructeur
-	public Monstre(Carte carte, TypesM type, Position pos) {
-		super(carte, pos, type.getPoints(), type.getPorteeVisuelle(), type.getPorteeDeplacement(), type.getPuissance(), type.getTir());
-		TYPE = type;
-		numTexture = TEX_MONSTRE;
-	}
-	
-	// Accesseurs
-	public TypesM getTYPE() { return TYPE; }
-	
-	// Méthodes
-	public String getStringType() {
-		return ("" + TYPE).toLowerCase();
-	}
+    private static final long serialVersionUID = -4592881709041950606L;
+    /**
+     * Type du Monstre
+     */
+    private final TypesM TYPE;
+    
+    /**
+     * Constructeur de Monstre
+     * @param carte
+     * @param type
+     * @param pos
+     */
+    public Monstre(Carte carte, TypesM type, Position pos) {
+        super(carte, pos, type.getPoints(), type.getPorteeVisuelle(), type.getPorteeDeplacement(), type.getPuissance(), type.getTir());
+        TYPE = type;
+        numTexture = TEX_MONSTRE;
+    }
+    
+    /**
+     * Accesseur du type
+     * @return TypesM
+     */
+    public TypesM getTYPE() { return TYPE; }
+    
+    /**
+     * Retourne le type du personnage sous forme de chaine de caractère
+     */
+    public String getStringType() {
+        return ("" + TYPE).toLowerCase();
+    }
 }
